66c81a3a24ebb6d7acb7cec9eff2b20235365892
jbargu <jure.grabnar@gmail.com>
2024-01-30 14:05:45 +0100

    Add images for Icons

--- src/main/java/games/arknova/ArkNovaConstants.java
+++ src/main/java/games/arknova/ArkNovaConstants.java
@@ -3,6 +3,19 @@ package games.arknova;
 public class ArkNovaConstants {
 
   public static final int MINIMUM_KIOSK_DISTANCE = 3;
+  public static final int STARTING_MONEY = 25;
+  public static final int MONEY_PER_ONE_BUILDING_HEX = 2;
+
+  public static final int MAXIMUM_APPEAL = 113;
+  public static final int MAXIMUM_CONSERVATION_POINTS = 41;
+  public static final int MAXIMUM_REPUTATION = 15;
+  public static final int MAXIMUM_X_TOKEN = 5;
+  public static final int MAXIMUM_WORKERS = 4;
+
+  public static final int[] MAXIMUM_BREAK = new int[] {9, 12, 15};
+
+  // Game ends after the score reach 100 (when CP marker and appeal marker cross each other)
+  public static final int TRIGGER_END_GAME_SCORE = 100;
 
   public enum Resource {
     MONEY,
@@ -11,5 +24,27 @@ public class ArkNovaConstants {
     REPUTATION,
     X_TOKEN,
     WORKER,
+  }
+
+  public enum Icon {
+    AFRICA,
+    EUROPE,
+    ASIA,
+    AMERICA,
+    AUSTRALIA,
+    BIRD,
+    PREDATOR,
+    HERBIVORE,
+    REPTILE,
+    PRIMATE,
+    BEAR,
+    PETTING_ZOO_ANIMAL,
+    SCIENCE,
+    ROCK, // not really a tag per-se, but they follow a similar logic
+    WATER; // not really a tag per-se, but they follow a similar logic
+
+    public String getImagePath() {
+      return String.format("data/arknova/icons/%s.png", this.name().toLowerCase());
+    }
   }
 }
